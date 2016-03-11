173e3c4c38d2518d053f83b33da9d476ae485c61
Gertjan Roke <gertjan_roke90@hotmail.com>
2016-03-11 21:27:58 +0100

    Update and rename UpdateUserRequest.php to UserRequest.php

---
+++ src/Http/Requests/UserRequest.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\User;
+use App\Http\Requests\Request;
+
+/**
+ * Update user request
+ * @package users
+ * @author Gertjan Roke
+ */
+class UpdateUserRequest extends Request
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        $user = \Auth::user();
+        if($user->hasRoles(['admin', 'super admin'])) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        $id = $this->route()->users;
+        $user = User::findOrFail($id);
+        switch($this->method())
+        {
+            case 'POST':
+            {
+                return [
+                        'name'      => 'max:255',
+                        'email'     => 'required|email|max:255|unique:users,email',
+                        'role'      => 'required',
+                        'password'  => 'required|min:5|max:255|confirmed',
+                    ];
+            }
+            case 'PUT':
+            case 'PATCH':
+            {
+                if(!$this->get('password'))
+                {
+                    return [
+                        'name'      => 'max:255',
+                        'email'     => 'required|email|max:255|unique:users,email,'.$user->id,
+                        'role'      => 'required',
+                        'password'  => 'min:5|max:255|confirmed',
+                    ];
+                }
+                else
+                {
+                    return [
+                        'name'      => 'max:255',
+                        'email'     => 'required|email|max:255|unique:users,email,'.$user->id,
+                        'role'      => 'required',
+                        'password'  => 'required|min:5|max:255|confirmed',
+                    ];
+                }
+            }
+            default:break;
+        }
+    }
+}
